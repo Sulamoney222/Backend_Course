05a93d4966094f38efb096b085f0a69e464d3184
Sulaimon <sulaimonnasiru06@gmail.com>
2025-06-16 10:35:00 +0100

    Initial commit

--- chapter-2/server.js
+++ chapter-2/server.js
@@ -7,5 +7,6 @@ const PORT = process.env.PORT || 5000
 app.listen(PORT, ()=>{
     console.log(`Servr has started on port: ${PORT}`); 
     console.log(`Servr has starte: ${PORT}`);  
+    console.log('Hello World');
     
 })
