1386fc5a6a42a0896ef4045ecb09c9219f39af59
Sulaimon <sulaimonnasiru06@gmail.com>
2025-06-26 19:55:10 +0100

    new changes

--- John-BackCourse/app.js
+++ John-BackCourse/app.js
@@ -5,6 +5,13 @@ const readFilePromise = util.promisify(readFile)
 const writeFilePromise = util.promisify(writeFile)
 
 const start = async ()=>{
+    try {
     const first = await readFilePromise( './texts/first.txt', 'utf8')
     const second = await readFilePromise( './texts/first.txt', 'utf8')
+    console.log(first, second);
+    
+    } catch (error) {
+        console.log(error);
+        
+    }
 }
