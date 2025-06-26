53231bcfefa7769b81a4d34581a3f6d0270e0e2c
Sulaimon <sulaimonnasiru06@gmail.com>
2025-06-26 20:09:26 +0100

    new changes

--- John-BackCourse/app.js
+++ John-BackCourse/app.js
@@ -1,17 +1,21 @@
+const { log } = require('console')
 const {readFile,writeFile} = require('fs')
 const util = require('util')
 
 const readFilePromise = util.promisify(readFile)
 const writeFilePromise = util.promisify(writeFile)
 
-console.log('World');
+
+console.log('What is my name? ');
 
 
 const start = async ()=>{
     try {
     const first = await readFilePromise( './texts/first.txt', 'utf8')
-    const second = await readFilePromise( './texts/second.txt', 'utf8')
-    console.log(first, second);
+    const second = await readFilePromise( './texts/third.txt', 'utf8')
+    console.log(first, second,
+        writeFilePromise('./texts/fourth.txt', 'What is happening in the fourth page')
+    )
     
     } catch (error) {
         console.log(error);
@@ -19,3 +23,4 @@ const start = async ()=>{
     }
 }
 start()
+console.log('World');
