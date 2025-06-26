6ec0d3e98e65049a807b4b42b8adf55cef743aab
Sulaimon <sulaimonnasiru06@gmail.com>
2025-06-26 19:37:59 +0100

    newq changes

--- John-BackCourse/app.js
+++ John-BackCourse/app.js
@@ -1,18 +1,3 @@
-const os = require('os')
-const {readFileSync,writeFileSync,readFile} = require('fs')
 
-const current = readFileSync('./texts/first.txt','utf8')
-console.log(current);
-writeFileSync(
-    './texts/third.txt',
-    'This is the third page'
-)
-readFile('./tests/third.txt,', 'utf8', (err,result)=>{
-    if(err){
-        return err
-    }
-    console.log(result);
-    
-})
 
 
