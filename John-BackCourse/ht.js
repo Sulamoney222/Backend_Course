0a22e184886891024b95f5560b0dc62970ddc1b0
Sulaimon <sulaimonnasiru06@gmail.com>
2025-06-28 11:51:35 +0100

    new changes

--- John-BackCourse/ht.js
+++ John-BackCourse/ht.js
@@ -4,16 +4,17 @@ const {readFileSync} = require('fs')
 const server = http.createServer((req,res)=>{
    
     const homepage = readFileSync('./public/index.html')
-     res.writeHead(200,{'content-type': 'text/html'})
+    const stylepage = readFileSync('./public/style.css')
+
+    if (req.url === '/') {
+        res.writeHead(200, {'content-type': 'text/html'})
+        res.write(homepage)
+        res.end()       
+    }
+    else if(req.url==='/'){
+        res.writeHead
+    }
     
-  if(req.url === '/'){
-  res.write(homepage)
-  }else if(req.url === '/about'){
-    res.write(`<h1 style= 'color:red'>Hello, this is fum</h1>`)
-  } else {
-    res.write(`<h1>Page not found!</h1>`)
-  }
-   res.end()
 })
 
 server.listen(5000,()=>{
