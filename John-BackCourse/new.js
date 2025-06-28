fe88ade1e99f04b5af99e9af38ec6ac5a8fd4350
Sulaimon <sulaimonnasiru06@gmail.com>
2025-06-28 16:54:16 +0100

    new changes

--- John-BackCourse/new.js
+++ John-BackCourse/new.js
@@ -1,1 +1,8 @@
-const express = require
+const app = require('express')()
+
+const PORT = 5000 || process.env
+
+app.listen(PORT, ()=>{
+    console.log(`Server is listening.... ${PORT}`);
+    
+})
