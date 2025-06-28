39b4e8f16c7f543f071a45dc4a0de12eca3781db
Sulaimon <sulaimonnasiru06@gmail.com>
2025-06-28 10:43:27 +0100

    new changes

--- John-BackCourse/ht.js
+++ John-BackCourse/ht.js
@@ -3,9 +3,9 @@ const http = require('http')
 const server = http.createServer((req,res)=>{
     res.writeHead(200,{'content-type': 'text/html'})
     
-  if(req.url = '/'){
+  if(req.url === '/'){
   res.write(`<h1 style= 'color:blue'>Hello, how are you doing? </h1>`)
-  }if(req.url = '/about'){
+  }if(req.url === '/about'){
     res.write(`<h1 style= 'color:red'>Hello, this is fum</h1>`)
   }
    res.end()
