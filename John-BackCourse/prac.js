150f49605c29ebda2a5e3e2b333ba995cd7ec656
Sulaimon <sulaimonnasiru06@gmail.com>
2025-06-28 09:54:01 +0100

    new changes

--- John-BackCourse/prac.js
+++ John-BackCourse/prac.js
@@ -1,2 +1,24 @@
-const {writeFile} = require('fs')
-const streams = require('stream')
+const {writeFileSync} = require('fs')
+const http = require('http')
+const fs = require('fs')
+const { error } = require('console')
+
+for (let index = 0; index < 10000; index++) {
+    writeFileSync(
+        './texts/s.txt', `New file ${index}`, {flag:'a'}
+    )
+    
+}
+
+const server = http.createServer()
+
+server.on('request', (req,res)=>{
+   const file = fs.createReadStream('./texts/s.txt')
+   file.pipe(res)
+   
+})
+
+server.listen(5000, ()=>{
+    console.log('server is starteing');
+    
+})
