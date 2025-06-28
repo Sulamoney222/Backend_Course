e1bb770ce8ad4df51740267d21af2953a4623724
Sulaimon <sulaimonnasiru06@gmail.com>
2025-06-28 10:36:15 +0100

    new changes

--- John-BackCourse/ht.js
+++ John-BackCourse/ht.js
@@ -4,11 +4,11 @@ const server = http.createServer((req,res)=>{
     res.writeHead(200,{'content-type': 'text/html'})
     
   if(req.url = '/'){
-res.write(`<h1 style= 'color:blue'>Hello, how are you doing? </h1>`)
+  res.write(`<h1 style= 'color:blue'>Hello, how are you doing? </h1>`)
   }if(req.url = '/about'){
     res.write(`<h1 style= 'color:red'>Hello, this is fum</h1>`)
   }
-  res.end()
+   res.end()
 })
 
 server.listen(5000,()=>{
