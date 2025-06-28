15c5a6aa8b17480a5008614025815c62e042120f
Sulaimon <sulaimonnasiru06@gmail.com>
2025-06-28 01:04:17 +0100

    new changes

--- John-BackCourse/streams.js
+++ John-BackCourse/streams.js
@@ -1,8 +1,17 @@
 const {writeFile} = require('fs')
+const {createReadStream} = require('fs')
 
-for (let index = 0; index < 1000; index++) {
-    writeFile(
-        './'
-    )
+// for (let index = 0; index < 1000; index++) {
+//     writeFile(
+//         './texts/big.txt', `Hello WORLD ${index}/n`,
+//         {flag:'a'}
+//     )
     
-}
+// }
+
+const stream = createReadStream('./texts/big.txt')
+
+stream.on('data', (result)=>{
+    console.log(result);
+    
+})
