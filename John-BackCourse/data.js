8f3f19344cf15aebf72d2f6532297e7ebc9de411
Sulaimon <sulaimonnasiru06@gmail.com>
2025-06-28 17:24:05 +0100

    new changes

---
+++ John-BackCourse/data.js
@@ -0,0 +1,27 @@
+
+const products = [
+    {
+        id:1,
+        name:'Nasiru Sulaimon',
+        age:23,
+        Skill: 'Developer'
+    },
+    {
+        id:2,
+        name:'Ibrahim Farouk',
+        age:25,
+        Skill: 'Affilate Marketer'
+    },
+    {
+        id:3,
+        name:'Olowolafe Chisam',
+        age:24,
+        Skill: 'UI UX Designer'
+    },
+      {
+        id:4,
+        name:'Ibironke Timileyin',
+        age:24,
+        Skill: 'Copywighter'
+    }
+]
--- John-BackCourse/new.js
+++ John-BackCourse/new.js
@@ -2,7 +2,10 @@ const app = require('express')()
 
 const PORT = 5000 || process.env
 
+  app.get('/', (req,res)=>{
+    res.status(200).json({id:1, name: 'Nasiru Sulaimon', age:23})
+  })
+
 app.listen(PORT, ()=>{
-    console.log(`Server is listening.... ${PORT}`);
-    
+    console.log(`Server is listening.... ${PORT}`);    
 })
