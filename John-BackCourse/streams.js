6ed3fe94700ca9b6556916f62a26c92afb67237b
Sulaimon <sulaimonnasiru06@gmail.com>
2025-06-28 01:38:55 +0100

    new changes

--- John-BackCourse/streams.js
+++ John-BackCourse/streams.js
@@ -1,5 +1,7 @@
 const {writeFile} = require('fs')
+const fs = require('fs')
 const {createReadStream} = require('fs')
+const https = require('http')
 
 // for (let index = 0; index < 1000; index++) {
 //     writeFile(
@@ -12,10 +14,29 @@ const {createReadStream} = require('fs')
 const stream = createReadStream('./texts/big.txt')
 
 stream.on('data', (result)=>{
-    console.log(result.BYTES_PER_ELEMENT);
+    console.log(result);
     
 })
 stream.on('error',(err)=>{
     console.log(err);
     
 })
+
+
+
+const server = https.createServer()
+
+server.on('request', (req,res)=>{
+  
+    const file = fs.createReadStream('./')
+    file.on('open', ()=>{
+        file.pipe()
+    })
+    file.on('error', (err)=>{
+        res.end(err)
+    })
+    
+})
+
+
+server.listen(5000)
