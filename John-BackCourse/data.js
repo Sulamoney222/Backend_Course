d2cfa3f9ba86aed88ad87c96b7a1ddb7782e1ae6
Sulaimon <sulaimonnasiru06@gmail.com>
2025-06-29 16:21:29 +0100

    new changes

--- John-BackCourse/data.js
+++ John-BackCourse/data.js
@@ -30,7 +30,8 @@ const people= [
     {id:1, name:'John'},
     {id:2, name:'Peter'},
     {id:3, name:'Michael'},
-    {id:4, name:'Anna'}
+    {id:4, name:'Anna'},
+     {id:1, name:'John'}
 ]
 
 module.exports.products = products
