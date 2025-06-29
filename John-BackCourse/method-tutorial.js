56854a12e394a78935e55ad3f953dec9d5e5a385
Sulaimon <sulaimonnasiru06@gmail.com>
2025-06-29 15:18:18 +0100

    new changes

---
+++ John-BackCourse/method-tutorial.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+
+//middleware
+app.use(express.static('./methods-public'))
+
+
+app.get('/',(req,res)=>{
+    res.send('Hello World')
+    
+})
+
+app.post('/login', (req,res)=>{
+    res.send('Hello, how are you doing?')
+})
+
+app.listen(5000, ()=>{
+    console.log(`Server is listening....`);
+    
+})
