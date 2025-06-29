8a88ebd66d4cdcb51d963a53c30565a894d73e8e
Sulaimon <sulaimonnasiru06@gmail.com>
2025-06-29 16:09:51 +0100

    new changes

--- John-BackCourse/method-tutorial.js
+++ John-BackCourse/method-tutorial.js
@@ -13,7 +13,11 @@ app.get('/',(req,res)=>{
 
 app.post('/login', (req,res)=>{
    const {name} = req.body
-   
+   if(name){
+   return res.status(200).send(`Welcome to our page ${name}`)
+   }else{
+     return res.status(400).send(`Pls input name!`)
+   }
     
 }) 
 
