1ca545349658cc3f6cce43b2f3c8dc1ad216d451
Sulaimon <sulaimonnasiru06@gmail.com>
2025-07-03 14:21:24 +0100

    new changes

--- John-BackCourse/method-tutorial.js
+++ John-BackCourse/method-tutorial.js
@@ -1,9 +1,12 @@
 const express = require('express')
+const { people } = require('./data')
 const app = express()
 
 //middleware
 app.use(express.static('./methods-public'))
 app.use(express.urlencoded({extended: false}))
+//parse json
+app.use(express.json())
 
 
 app.get('/',(req,res)=>{
@@ -19,7 +22,19 @@ app.post('/login', (req,res)=>{
      return res.status(400).send(`Pls input name, blank page!`)
    }
     
-}) 
+})
+
+app.get('/api/people', (req,res)=>{
+     res.status(200).json({success:true, data:people})
+})
+app.post('/api/people', (req,res)=>{
+  const {name} = req.body
+  if(!name){
+    return res.status(400).json({success:false, msg: 'Pls provide credentials'})
+  }
+  res.status(200).json({success:true,person:name})
+
+})
 
 app.listen(5000, ()=>{
     console.log(`Server is listening....`);
